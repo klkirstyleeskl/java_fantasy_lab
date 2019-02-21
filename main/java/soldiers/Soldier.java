4ce76c1940e50d8381e513c90b7d1ee77f4a98a3
klkirstyleeskl <k.lees@btinternet.com>
2019-02-21 11:16:23 +0000

    passes barbarian getters and setters

--- main/java/soldiers/Soldier.java
+++ main/java/soldiers/Soldier.java
@@ -1,4 +1,43 @@
 package soldiers;
 
-public class Soldier {
+public abstract class Soldier {
+    String name;
+    int health;
+    Weapon weapon;
+    Armour armour;
+
+    public Soldier(String name, int health, Weapon weapon, Armour armour) {
+        this.name = name;
+        this.health = health;
+        this.armour = armour;
+        this.weapon = weapon;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public Armour getArmour() {
+        return armour;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public void setArmour(Armour armour) {
+        this.armour = armour;
+    }
 }
